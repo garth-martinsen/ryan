7ff027a9ebfd885c583f2dca76789a0b39bd8a09
Garth Martinsen <garth.martinsen@gmail.com>
2025-08-12 22:07:33 -0700

    Put conditional handlers in according to the number of active threads, This causes the first threads target (handler) to be "handle_adc_client" and the next to be "handle_scheduler_client". I created two handler methods with the same names.Each handler only deals the messages from their client. This appears to be working. Future work, receive msgs from scheduler_client and send them to the other adc_client  to respond.

--- clientserver/db_server.py
+++ clientserver/db_server.py
@@ -19,7 +19,7 @@ SERVER = '192.168.254.19'   #the host computer running in python3
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECTED"
-#client requests/notification to server...
+#adc_client requests/notifications to server...
 CFG_IDS                = '10'
 LUT0                       = '20'
 LUT1                       = '30'
@@ -70,6 +70,9 @@ stepdict[0]= [3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4
 stepdict[1]=[6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9.0]
 stepdict[2] =[9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10.0, 10.1, 10.2, 10.3, 10.4, 10.5, 10.6, 10.7, 10.8, 10.9, 11.0, 11.1, 11.2, 11.3, 11.4, 11.5, 11.6, 11.7, 11.8, 11.9, 12.0, 12.1, 12.2, 12.3, 12.4, 12.5, 12.6, 12.7, 12.8, 12.9, 13.0, 13.1, 13.2, 13.3, 13.4, 13.5]
 
+# clientdict={}  # can lookup conn for each client: adc_client, scheduler_client
+clients=["handle_adc_client", "handle_scheduler_client"]
+
 def timestamp(self):
     '''Returns local time as string, eg: YYYY-mm-DD HH:MM:SS'''
     dt = time.localtime()
@@ -89,7 +92,7 @@ def timestamp(self):
 #         conn.send(payload.encode(FORMAT))
 #         print( "sending to client: ", payload)
         
-def handle_client(conn: socket, addr):
+def handle_adc_client(conn: socket, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     client_initiated_msgs.clear()
     client_can_respond =False
@@ -102,74 +105,95 @@ def handle_client(conn: socket, addr):
 
         msg_len = int(header)
         msg = conn.recv(msg_len).decode(FORMAT)
+        
         if msg == DISCONNECT_MESSAGE:
             connected = False
             break
         else:
-            if msg == '10':
-                obj= {"purpose": msg, "cfg_ids": dc.cfg_ids }
+            jsonx=json.loads(msg)
+            purpose = jsonx['purpose']
+            if purpose == '0':
+                greeting = jsonx['greet']
+                client_id= jsonx["client_id"]
+                print(f" Greeting: {greeting}  Client_id: {client_id}")
+                obj = {"purpose": '1', "purpose" : "received" }
                 payload = json.dumps(obj)
                 conn.send(payload.encode(FORMAT))
-            elif msg == '20' :
-                obj={"purpose": msg, "chan" : 0, "lut" : dc.luts[0]}
+            
+            
+            if purpose == '10':
+                obj= {"purpose": '11', "cfg_ids": dc.cfg_ids }
                 payload = json.dumps(obj)
                 conn.send(payload.encode(FORMAT))
-            elif msg == '30' :
-                obj={"purpose": msg, "chan" : 1, "lut" : dc.luts[1]}
+                #print(f" purpose: {purpose}  clientdict: {clientdict} ")
+            elif purpose == '20' :
+                obj={"purpose": '21', "chan" : 0, "lut" : dc.luts[0]}
                 payload = json.dumps(obj)
                 conn.send(payload.encode(FORMAT))
-            elif msg == '40' :
-                obj={"purpose": msg, "chan" : 2, "lut" : dc.luts[2]}
-                payload = json.dumps(obj)
-                conn.send(payload.encode(FORMAT))
-            elif msg == '50' :
-                client_can_respond=True
-                print(f" Client can now respond to Server Cmds:  {client_can_respond}")
 
-            else:   # msg is not just a number...
-                # rqsts from someone will be delegated to client to respond
-                jsonx = json.loads(msg)
-                intent = jsonx['purpose']
-                if intent == '100': #someone requested a measure
-                    if client_can_respond:
-                        conn.send(jsonx)
-                elif intent == '200': # someone requested a calibration
-                    if client_can_respond:
-                        conn.send(jsonx)
-                elif intent == '101':
-                    print("server received measurement response...")
-                    save_measurement(jsonx)
-                elif intent == '201':
-                    print("201:server received calibration response...")
-                    save_calibration(jsonx)
-        # temporary calibrate msg to client
-#         obj={"purpose": '200', "chan" : 0, "vin" : 3.00 }  # vin will be input by user replacing this.
-#         payload = json.dumps(obj)
-#         conn.send(payload.encode(FORMAT))
+            elif purpose == '30' :
+                obj={"purpose": '31', "chan" : 1, "lut" : dc.luts[1]}
+                payload = json.dumps(obj)
+                conn.send(payload.encode(FORMAT))
+
+            elif purpose == '40' :
+                obj={"purpose": '41', "chan" : 2, "lut" : dc.luts[2]}
+                payload = json.dumps(obj)
+                conn.send(payload.encode(FORMAT))
+
+            elif purpose == '50' :
+                client_can_respond=True
+                print(f" Client can now respond to Server Cmds:  {client_can_respond} ")
+                #print(f" purpose: {purpose}  clientdict: {clientdict} ")
+            elif purpose == '101':
+                '''server received measurement response.
+                Tells DataController to save it to db. Notify scheduler.'''
+                dc.save_measurement(jsonx)      
+            elif purpose == '201':
+                print("201:server received calibration response...")
+                dc.save_calibration(jsonx)
+
+def handle_scheduler_client(conn: socket, addr):
+    connected = True
+    while connected:
+        header = conn.recv(HEADER).decode(FORMAT)  # blocking line
+        if not header:
+            continue
+        msg_len = int(header)
+        msg = conn.recv(msg_len).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+            break
+        else:
+            jsonx=json.loads(msg)
+            purpose = jsonx['purpose']
+            if purpose == '100': #someone requested an adc measure
+                if client_can_respond:
+                    handler_adc_client(msg)
+                    #conn.send(jsonx)
+            elif purpose == '200': # someone requested an adc calibration
+                if client_can_respond:
+                    handler_adc_client(msg)
+                  #conn.send(jsonx)  # send to adc_client
         
         print(f"[MESSAGE RECEIVED AT SERVER:][{addr}] {msg} ")
+        #print(f" clientdict: {clientdict}")
     conn.close()
  
-def save_measurement(self, json1):
-    ''' request datacontroller to save  a measurement. Timestamp and type are handled in dbi'''
-    print(f"Called dbsvr.save_measurement() with msg: {json1}")
-    dc.save_measurement(json1)
 
-def save_calibration(json1):
-    ''' Sets type to calibration ('c'), creates lists for column names (cols) and values (vals).
-       Requests datacontroller to save_calibration. Timestamp and type are handled in dbi'''
-    print(f"Called dbsvr.save_calibration() with msg: {json1}")
-    dc.save_calibration(json1)
-
-                
 def start():
     server.listen()
     while True:
         conn, addr = server.accept()  # blocking line
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        #         client_handler = clients[ threading.active_count() -1]
+        #         print (f" client_handler: {client_handler}")
+        if threading.active_count()-1 == 0:
+            thread = threading.Thread(target=handle_adc_client, args=(conn, addr))
+        else:
+            thread = threading.Thread(target=handle_scheduler_client, args=(conn, addr))
 
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
-
+        print(f" thread: {thread}")
 print("[STARTING] Server is starting ...")
 start()
