d4ae7466ba5ecf5c8ea1683a6a2dc8deb1979eee
Garth Martinsen <garth.martinsen@gmail.com>
2025-09-05 21:55:38 -0700

    Changed to use AI advice: added three global vars: adc_conn, gui_conn, sender_id. Removed two dicts: sockets_by_addr, client_names_by_addr , Added sender_id property to every msg.Gave each of adc_conn and gui_conn the value of conn when the thread was active. Added logic to branch on purpose and client_id for 100-201 msgs. appended the 100, 100 msgs to list: msgs_for_adc. appended the 101,201 msgs to list: msgs_for_gui_client. When proper thread is active. If any msgs in list, pop and send according to the final recipient.

--- clientserver/db_server.py
+++ clientserver/db_server.py
@@ -19,15 +19,17 @@ server.bind(ADDR)
 
 #global vars which change upon the receipt of each client msg.
 msg=''
-conn=None
-addr=None
+adc_conn = None
+gui_conn  = None
+sender_id = 'server'
+
 
 class  db_server:
     def __init__(self): 
         self.dc = DataController((1,2,3))  # initialize with your project's cfg_ids here instead of (1,2,3)
         self.clients_READY               = dict()
-        self.sockets_by_addr             = dict()
-        self.client_names_by_addr   = dict()
+        #self.sockets_by_addr             = dict()
+        #self.client_names_by_addr   = dict()
         self.responses_per_rqst        = dict()
         self.load_responses()            # loads the responses_per_rqst dict
         self.msgs_for_gui_client         = []
@@ -39,16 +41,16 @@ class  db_server:
         # for 100 <= purpose <= 500 : save data to db and forward to the other client
         #self.responses_per_rqst = dict()
         #self.responses_per_rqst[0]   = {"purpose":1, "greet": "Welcome!"}
-        self.responses_per_rqst[10] = {"purpose":11, "cfg_ids": tuple(self.dc.cfg_ids)}
-        self.responses_per_rqst[20] = {"purpose":21, "chan":0, "lut": self.dc.luts[0]}
-        self.responses_per_rqst[30] = {"purpose":31, "chan":1, "lut":  self.dc.luts[1]}
-        self.responses_per_rqst[40] = {"purpose":41, "chan":2, "lut":  self.dc.luts[2]}
-        self.responses_per_rqst[50] = {"purpose":51, "status": self.clients_READY }
-        self.responses_per_rqst[60] = {"purpose":61, "config": self.dc.cfg }
+        self.responses_per_rqst[10] = {"purpose":11, "sender_id":sender_id,"cfg_ids": tuple(self.dc.cfg_ids)}
+        self.responses_per_rqst[20] = {"purpose":21, "sender_id":sender_id, "chan":0, "lut": self.dc.luts[0]}
+        self.responses_per_rqst[30] = {"purpose":31,  "sender_id":sender_id,"chan":1, "lut":  self.dc.luts[1]}
+        self.responses_per_rqst[40] = {"purpose":41,  "sender_id":sender_id, "chan":2, "lut":  self.dc.luts[2]}
+        self.responses_per_rqst[50] = {"purpose":51,  "sender_id":sender_id,"status": self.clients_READY }
+        self.responses_per_rqst[60] = {"purpose":61,  "sender_id":sender_id, "config": self.dc.cfg }
 
     def send_lut(self, purpose, chan, lut):
         global msg, conn, addr
-        obj= f'purpose": {purpose}, "chan":chan, "lut": lut'
+        obj= { "purpose": purpose,  "sender_id": sender_id, "chan":chan, "lut": lut}
         msg=json.dumps(obj)
         #print(f"msg: {msg}")
         conn.send(msg)
@@ -76,12 +78,11 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 # socket method...
-def handle_client(conn1: socket, addr1):
-    global msg, conn, addr
+def handle_client(conn: socket, addr):
+    global adc_conn, gui_conn
     print(f"[NEW CONNECTION] {addr} connected.")
     connected = True
-    conn=conn1
-    addr = addr1
+
     while connected:
         header = conn.recv(HEADER).decode(FORMAT)  # connecting blocking line===========
         #print(f" while connected: header: {header}")
@@ -95,44 +96,46 @@ def handle_client(conn1: socket, addr1):
             break
         
        #print(f" msg received at server: {amsg}")
-       # turn string into dict using json and get info from the msg
+       # turn string into dict using json and get purpose and sender from the msg
         msg= json.loads(amsg)
         purpose = int(msg["purpose"])
+        sender_id = msg["sender_id"]
+        if sender_id == 'adc_client':
+            adc_conn = conn
+        else:
+            gui_conn = conn
+        #print(f" conn:  {conn}  adc_conn: {adc_conn}  gui_conn: {gui_conn}")
         if purpose == 0:
-            client_id= msg["client_id"]
-            dbs.client_names_by_addr[addr]=client_id
-            dbs.sockets_by_addr[addr]=conn
-            rspns ={"purpose": 1, "greet": "Svr welcomes ", "client_id" :  client_id }
+            sender_id= msg["sender_id"]
+            rspns ={"purpose": 1, "greet": "Svr welcomes ", "sender_id" :  sender_id }
             rmsg = json.dumps(rspns)
             #print(f" type: {type(rmsg)}   rmsg: {rmsg}")
             conn.send(rmsg.encode(FORMAT))
         # execute the correct action given the purpose...    
         print(f"\n[MESSAGE RECEIVED][{addr}] {msg} ")
-        print(f" preparing msg : {purpose+1 }  responding to msg : {purpose}")
-        #print(f"          client_names: {dbs.client_names_by_addr}  \n          sockets: {dbs.sockets_by_addr}\n")
+        print(f" preparing msg : {purpose+1 }  to respond to msg : {purpose}")
         if 0 < purpose < 60:
             rspns = json.dumps(dbs.responses_per_rqst[purpose])   # given rqst with purpose, returns rspns ( purpose+1)
             conn.send(rspns.encode(FORMAT))
         elif purpose == 60:
-            obj = {"purpose": 61, "configs0" : dbs.dc.cfg[0][:-1], "configs1": dbs.dc.cfg[1][:-1], "configs2": dbs.dc.cfg[2][:-1]}
-            #print(f"configs: {obj}")
+            obj = {"purpose": 61, "sender_id": sender_id, "configs0" : dbs.dc.cfg[0][:-1], "configs1": dbs.dc.cfg[1][:-1], "configs2": dbs.dc.cfg[2][:-1]}
             rmsg = json.dumps(obj)
             conn.send(rmsg.encode(FORMAT))
-        elif purpose in [100,200] :
+        if purpose in [100,200]  and sender_id == 'gui_client':
             # purpose x00 is coming from gui_client.. put it into msgs_for_adc_client...
             dbs.msgs_for_adc_client.append(msg)
             print(f"msgs_for_adc_client : {dbs.msgs_for_adc_client}")
             # if there are any msgs for gui, pop and send while its conn is in active thread.
-            if len(dbs.msgs_for_gui_client) > 0:
+            if dbs.msgs_for_gui_client :
                 gui_msg = dbs.msgs_for_gui_client.pop()
                 conn.send(gui_msg)
-        elif purpose in [101, 201]:
+        if purpose in [101, 201] and sender_id == 'adc_client':
             # purpose x01 is coming from adc_client... put msg into msgs_for_gui_client...
             dbs.save_to_database( purpose, msg)
             dbs.msgs_for_gui_client.append(msg)
             print(f"msgs_for_gui_client : {dbs.msgs_for_gui_client}")
             # if there are any msgs for adc, pop and send.
-            if len(dbs.msgs_for_adc_client ) > 0:
+            if dbs.msgs_for_adc_client :
                 adc_msg = dbs.msgs_for_adc_client.pop()
                 conn.send(adc_msg)
             
@@ -149,7 +152,6 @@ def start():
 
 # --------- calls ------------------
 dbs = db_server()
-#dbs.load_responses()
 
 print("[STARTING] Server is starting ...")
 
