3b52ddd41aa981b18e95e2e88ac5863f6925a3ee
Garth Martinsen <garth.martinsen@gmail.com>
2025-09-05 21:31:10 -0700

    removed some comments.

--- clientserver/data_controller_config.py
+++ clientserver/data_controller_config.py
@@ -11,8 +11,3 @@ lsb = 4 / pow(2, 15)
 circuits = ["C42", "C84", "C126"]  # indexed by [0,1,2]
 # dbpath = "/Users/garth/Programming/MicroPython/usb/ryan/voltage_divider/data/db_rt"
 dbpath = "/Users/garth/DIST/clientServer/ryan/clientserver/data/db_rt"
-
-
-# dummy stuff for testing:
-# in future, this will be as measured by adc given input volts.
-# replaced by randomized stuff in calibrate function.
