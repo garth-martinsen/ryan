4993dae662c09b25a082847958ff5fe67187bfd8
Garth Martinsen <garth.martinsen@gmail.com>
2025-11-04 20:35:29 -0800

    Added MESSAGES map. added steps for each channel

--- clientserver/data_controller_config.py
+++ clientserver/data_controller_config.py
@@ -7,14 +7,20 @@ Lut_Limits = namedtuple(
 )
 
 
-lsb = 4 / pow(2, 15)
+
 circuits = ["C42", "C84", "C126"]  # indexed by [0,1,2]
 dbpath = "/Users/garth/DEV/ryan/clientserver/data/rt_db"
 
-MESSAGE_PURPOSES ={0: "HELLO", 1: "WELCOME CLIENT", 10: "rqst_cfg_ids", 11:"cfg_ids",
-                   20: "lut0_rqst", 21: "lut0",  30:"lut1_rqst", 31: "Lut1", 40: "lut2_rqst", 41: "lut2",
-                   50: "READY", 51: "READY_OK",  60: "config_rqst", 61: "configs", 100: "rqst_measure_chan",
-                   101: "chan_measure",  150: "rqst_schedule_measure", 175: "get_meas_records",
+MESSAGE_PURPOSES ={0: "HELLO", 1: "WELCOME CLIENT", 10: "rqst_cfg0", 11:"cfg0",
+                   12: "rqst_cfg1", 13: "cfg1",  14:"rqst_cfg2", 15: "cfg2",
+                   50: "READY", 51: "READY_OK",  100: "rqst_measure_chan",
+                   101: "measure_chan",  150: "rqst_schedule_measure", 175: "get_meas_records",
                    176: "measurement_records",  200: "rqst_calib_chan_vin",   201: "calib_chan_vin",
-                   250: "rqst_step_calib",  275: "get_calib_records", 276: "calib_records",
-                   300: "get_lut_chan",  400: " get_last_keep"}
+                   250: "rqst_step_calib",  275: "get_calib_records", 276: "calib_records"}
+# Note:
+# 1. purpose 150 results in many 101 responses and purpose 250 results in many 201 responses
+# 2. purpose 175 results in a list<BMS records> and purpose 275 results in a list<BMS records>
+
+C42_steps = [x/10 for x in range(30,46)]
+C84_steps = [x/10 for x in range(60, 91)]
+C126_steps= [x/10 for x in range(90,136)]
