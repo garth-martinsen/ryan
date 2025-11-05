605ffec8a89c5d8c89529b5cc3f0b4f88edc8592
Garth Martinsen <garth.martinsen@gmail.com>
2025-11-04 20:42:16 -0800

    small changes.

--- clientserver/database_interface_config.py
+++ clientserver/database_interface_config.py
@@ -3,9 +3,8 @@
 from collections import namedtuple
 
 # named tuples
-Lut_Limits = namedtuple(
-    "Lut_Limits", ("circuit_name", "lower_limit", "upper_limit", "length")
-)
+Lut_Limits = namedtuple( "Lut_Limits", ("circuit_name", "lower_limit", "upper_limit", "length"))
+
 CONFIG_FIELDS =    (
         "id",
         "owner",
@@ -28,23 +27,18 @@ CONFIG_FIELDS =    (
         "LUT_CALIBRATED",
         "LUT",
     )
-Config = namedtuple(
-    "Config",
-    CONFIG_FIELDS
-)
-Short_Record = namedtuple(
-    "Short_Record",
-    ("id", "owner", "app_desc", "version_desc", "channel_id", "channel_desc"),
-)
+Config = namedtuple( "Config", CONFIG_FIELDS)
 
-BMS = namedtuple(
-    "BMS",
-    (
+Short_Record = namedtuple( "Short_Record", ("id", "owner", "app_desc", "version_desc", "channel_id", "channel_desc"))
+
+BMS_FIELDS=(
+        "id",
         "cfg_id",
-        "meas_type",
+        "type",
         "timestamp",
         "a2d_mean",
         "a2d_sd",
+        "sample_sz",
         "vm",
         "vb",
         "vin",
@@ -53,11 +47,11 @@ BMS = namedtuple(
         "sample_period",
         "store_time",
         "gate_time",
-    ),
-)
+    )
 
-# db_path ='/Users/garth/Programming/MicroPython/usb/ryan/voltage_divider/data/rt_db'
-db_path = "/Users/garth/DIST/clientServer/ryan/clientserver/data/rt_db"
+BMS = namedtuple( "BMS", BMS_FIELDS)
+
+db_path = '/Users/garth/DEV/ryan/clientserver/data/rt_db'
 # Schema CONFIG: CREATE TABLE CONFIG (ID INTEGER PRIMARY KEY , channel_id integer,CHANNEL_DESC VARCHAR  VERSION_ID INTEGER NOT NULL, VERSION_DESC VARCHAR,  TIMESTAMP INTEGER,  MOSFET_ID INTEGER, MOSFET_TYPE VARCHAR, TEMPC REAL, R1 REAL, R2 REAL, RP REAL, RG REAL, LUT_CALIBRATED INTEGER, LUT VARCHAR);
 # Schema BMS: CREATE TABLE BMS ( id integer primary key, cfg_id integer, timestamp integer,  a2d real, a2d_sd real, vm real, vb real, keep varchar, sample_period real, store_time real, gate_time real );
 # example BMS: 1,1,1750436348, 20005.34, 2.123, 2.111, 3.465,[20045,20046,20047,...],0.00156,0.000444,1.003
