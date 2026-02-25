7792e19ca5b4bac8de1459a9ac0c00cff7f7ca36
Garth Martinsen <garth.martinsen@gmail.com>
2026-02-25 00:58:22 -0800

    fixed some bugs in client_data_store.py

--- simplewebsocket/client_data_store.py
+++ simplewebsocket/client_data_store.py
@@ -18,6 +18,7 @@ class DataStore:
             self.show_data_store()    
         else:
             jmsg=json.loads(msg)
+            #TODO 4: Find out what in msg that is breaking json.loads(msg) for the adc sd cmd.
             message = Message(jmsg[0],jmsg[1], jmsg[2], jmsg[3], jmsg[4], jmsg[5], jmsg[6], jmsg[7])
             #print(f"message: {message}")
             if message.purpose == 100:
@@ -29,18 +30,17 @@ class DataStore:
                 self.lut_timestamps = self.extract_lut_timestamps(message)
                 self.configurations = self.extract_configs(message)
                 
-      def extract_luts(self, message):
-         #TODO 1: implement extract_luts(self, message)  see test_gui_client for hints how to implement this method
-         print(f" TBD work in progress  to extract_luts() from {message}")
+    def extract_luts(self, message):
+        #TODO 1: implement extract_luts(self, message)  see test_gui_client for hints how to implement this method
+        print(f" TBD work in progress  to extract_luts() from {message}")
 
     def extract_lut_timestamps(self, message):
         #TODO 2: implement extract_lut_timestamps(self, message) see test_gui_client for hints how to implement this method
-
-         print(f" TBD work in progress to extract_lut_timestamps from {message}")
+        print(f" TBD work in progress to extract_lut_timestamps from {message}")
     
     def extract_configs(self, message):
-        #TODO 2: implement extract_configs(self, message) see test_gui_client for hints how to implement this method
-         print(f" TBD work in progress to extract_configs from {message}")
+        #TODO 3: implement extract_configs(self, message) see test_gui_client for hints how to implement this method
+        print(f" TBD work in progress to extract_configs from {message}")
     
     def show_data_store(self):
         print()
@@ -48,20 +48,20 @@ class DataStore:
         print("Configuration: ")
         for c in self.configurations:
             print(c)
-            print(("-------------------")
+            print("-------------------")
             print()
    
         print("Luts: ")
         for lu in self.luts:
             print(lu)
-            print(("-------------------")
+            print("-------------------")
             print()
   
  
-         print("Lut_timestamps: ")
+        print("Lut_timestamps: ")
         for lu in self.lut_timestamps:
             print(lu)
-            print(("-------------------")
+            print("-------------------")
             print()
 
         print("Measurements: ")
@@ -70,11 +70,10 @@ class DataStore:
                 print("-------------------")
         print()
  
- print("Calibrations: ")
+        print("Calibrations: ")
         for c in self.calibrations:
             print(c)
             print("----------------------")
-        for c in self.configurations
   
   
   
