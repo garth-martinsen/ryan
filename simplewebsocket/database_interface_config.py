32537c7486ab263a8b1c39f3efc4b28f8adb23f4
Garth Martinsen <garth.martinsen@gmail.com>
2026-03-11 20:58:13 -0700

    Removed mosfet and mosfet_type from namedtuple("Config")...Added purposes dict to give  db_cmds meanings.

--- simplewebsocket/database_interface_config.py
+++ simplewebsocket/database_interface_config.py
@@ -15,28 +15,71 @@ CONFIG_FIELDS =    (
         "version",
         "version_desc",
         "creation_time",
-        "mosfet",
-        "mosfet_type",
         "tempC",
         "ADC_GAIN",
         "ADC_SAMPLE_RATE",
+        "C1",
         "r1",
         "r2",
-        "rp",
-        "rg",
         "LUT_CALIBRATED",
-        "LUT",
         "LUT_TS"
     )
 
-Config = namedtuple( "Config", CONFIG_FIELDS)                                                                         #21 fields
+Config = namedtuple( "Config", CONFIG_FIELDS)                                                                         #17 fields
 
 Abbrev= namedtuple( "Abbrev", ("id", "owner", "app_desc", "version_desc", "channel_id", "channel_desc"))    #6 fields
 
-BMS = namedtuple("BMS", ('id', 'timestamp', 'type', 'chan',  'cfg_id', 'a2d',  'a2d_sd', 'sample_sz',  'vin','vm', 'vb',  'error', 'store_time', 'gate_time', 'sample_period',  'keep') )   #16 fields
+# fields 0 -6 populated by ADC ,  fields 7-11 populated by calculations by (gui_client or databaseInterface)
+BMS_FIELDS = (
+    'id',
+    'timestamp',
+    'type',
+    'chan',
+    'vin',
+    'store_time',
+    'sample_period',
+    'a2d_mean',
+    'a2d_sd',
+    'vm',
+    'vb',
+    'error',
+)
 
-LUT_INFO =namedtuple("LUT_INFO", ("LUT", "LUT_TS") )
+BMS = namedtuple("BMS", BMS_FIELDS )   #12 fields
+
+LUT =namedtuple("LUT", ("vm", "vin") )
                
-db_path = '/Users/garth/DEV/ryan/clientserver/data/rt_db'
+#db_path = '/Users/garth/DEV/ryan/clientserver/data/rt_db'
+db_path = '/Users/garth/DEV/ryan/simplewebsocket/data/rt_db'
+
+#message purposes: note that responses from the db or adc will have 1 added eg : 100 -> 101 etc.
+purposes = dict()
+purposes[100]='request_measure'
+purposes[101]='response_measurement'
+purposes[150]= 'past_measurements_0'
+purposes[152]= 'past_measurements_1'
+purposes[154]= 'past_measurements_2'
+purposes[200]='request_calibrate'
+purposes[201]='response_calibration'
+purposes[250]='request_past_calibrations_0'
+purposes[252]='request_past_calibrations_1'
+purposes[254]='request_past_calibrations_2'
+purposes[300]= 'save_config'
+purposes[310]= 'request_config'
+purposes[311]= 'response_config'
+purposes[350]= 'save_lut'
+purposes[360]= 'request_lut_0'
+purposes[361]= 'responselut_0'
+purposes[362]= 'request_lut_1'
+purposes[363]= 'response_lut_1'
+purposes[364]= 'request_lut_2'
+purposes[365]= 'response_lut_2'
+purposes[370]= 'request_lut0_timestamp'
+purposes[371]= 'response_lut0_timestamp'
+purposes[372]= 'request_lut1_timestamp'
+purposes[373]= 'response_lut1_timestamp'
+purposes[374]= 'request_lut2_timestamp'
+purposes[375]= 'response_lut2_timestamp'
+
 
 # join example: select cfg.id, cfg.owner, cfg.app_desc, cfg.channel_desc, cfg.version_desc, bms.timestamp, bms.a2d, bms.a2d_sd, bms.vm, bms.vb, bms.keep, bms.sample_period, bms.store_time, bms.gate_time  from CONFIG as cfg INNER JOIN  BMS as bms on cfg.id=bms.id;
