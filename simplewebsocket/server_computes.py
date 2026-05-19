adafb393d4da09acec217931a76d96a67f3483bd
Garth Martinsen <garth.martinsen@gmail.com>
2026-05-19 16:02:16 -0700

    updated the vd_fracts to agree with the resistors in latest PCB.

--- simplewebsocket/server_computes.py
+++ simplewebsocket/server_computes.py
@@ -4,11 +4,13 @@ import math
 
 samps = [22657, 22654, 22651, 22653, 22655, 22657, 22651, 22657, 22651, 22655, 22657, 22653, 22656, 22658, 22656, 22658, 22657, 22655, 22653, 22655, 22655, 22655, 22655, 22656, 22651, 22658, 22656, 22658, 22658, 22653, 22652, 22658, 22658, 22653, 22655, 22657, 22654, 22651, 22653, 22655, 22657, 22651, 22657, 22651, 22655, 22657, 22653, 22656, 22658, 22656, 22658, 22657, 22657, 22654, 22651, 22653, 22655, 22657, 22651, 22657, 22651, 22655, 22657, 22653]
 lsb=125e-6
-vd_fracts=[0.688128140703518, 0.313249211356467, 0.248189762796504]
+#vd_fracts=[0.688128140703518, 0.313249211356467, 0.248189762796504]
+vd_fracts=[0.749814585908529, 0.33633904418395, 0.250186335403727]
 
 class ServerComps:
     def __init__(self):
-        pass
+           self.oksd = 3.5
+        
         
     def avg(self, alst):
         return sum(alst)/len(alst)
@@ -24,7 +26,10 @@ class ServerComps:
         m=self.avg(samples)
         sd = self.std_dev(m, samples)   # sd of a2d samples
         # keep will be used for stats after discarding outliers...
-        keep = [x for x in samples if abs(x-m) <  3*sd]
+        if sd < self.oksd:
+           keep = samples
+        else:
+           keep = [x for x in samples if abs(x-m) <  3*sd]
         m=self.avg(keep)
         sd = self.std_dev(m,keep)
         vm_m  = m* lsb                           
