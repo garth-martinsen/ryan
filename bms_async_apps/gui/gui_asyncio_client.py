09d1889acec7ac53ec98ff24b26b095d6d65111b
Garth Martinsen <garth.martinsen@gmail.com>
2026-06-22 22:53:55 -0700

    in gui/gui_asyncio_client.py: added a funct_desc dict to gui_client can prompt user to input correct msg elements.Function: show_dbi_functions lists t    hem. Added input prompts depending on the code(command) the msg will support.  Added tabs in print outs reflecting msg sent or received to stand out to     the reader.  Ensured that the readline was first in the while True loop, then the outgoing msg prep and writing. Future: figure out how to close the w    riter correctly.  For now just commented out the write statements to keep the connection going. Timestamps are sent to db as time.time(). New function:     human_timestamp(float) will present the db timestamp values in human readable form.

--- bms_async_apps/gui/gui_asyncio_client.py
+++ bms_async_apps/gui/gui_asyncio_client.py
@@ -4,11 +4,21 @@ import asyncio
 import json
 import time
 
+
 INCOMING_MSG = namedtuple("INCOMING_MSG", ("SENDER", "RECEIVER","TIMESTAMP", "MSGID", "CODE", "TYPE","CHAN","SAMP_SZ","DISCARD_SZ","KEEP_SZ","A2D_MEAN","VM_MEAN","VM_SD","VB","VIN","ERROR"))
 OUTGOING_MSG = namedtuple("OUTGOING_MSG", ("SENDER", "RECEIVER","TIMESTAMP",  "CODE", "TYPE","CHAN","VIN"))
 msg = 'GUI, ADC, 2026-5-20  17:40:27,  {code} , {atype} , {chan},  {vin} '  
 
 #global vars
+funct_desc = {300: 'save_config(  msg : Config )',       310: 'get_config( chan : int )',
+                        320:'save_to_bms( msg :BMS )' ,         330: 'list_bms( chan:int, atype:str) ',
+                        340: 'get_bms_a2d_samples( bms_id : int)', 350: 'get_lut( chan:int)',
+                        360: 'get_lut_timestamp( chan:int )',     370:'update_lut_pair(  _id:int,   vm:float,   vin:float)' ,
+                        380: 'update_lut_timestamp( chan:int )', 390: 'get_vd_fracts( )'  }
+
+def show_dbi_functions():
+    for k,v in funct_desc.items():
+        print(k,v)
 
 def _timestamp():
         """Returns local time as string, eg: YYYY-mm-DD HH:MM:SS. Note: for hour, min, sec less than 10, must have leading zero eg: 09:05:07"""
@@ -24,21 +34,28 @@ def identify():
 
 def get_msg():
     ''' Formats msg with cmd values to the Server'''
-    print("include values for receiver, sender,  msgid, code, type, chan, vin as needed. If not needed just hit return with no entry eg: chan and vin ")
+    # print("Respond to promps. They will change depending on code (command). ")
     ts = _timestamp()
     receiver = input("RECEIVER: " )
     sender = input("SENDER: " )
     code = int(input("CODE: " ))
     msg =  {"RECEIVER": receiver , "SENDER" : sender, "TIMESTAMP": ts, "CODE": code}
+    msg["ARGLIST"]=[]
     if code == 175:
-        msg["PERIOD" ]= int(input("period"))
-        msg["REPS"]  =int( input("repetitions"))
+        msg["PERIOD" ]= int(input("period: "))
+        msg["REPS"]  =int( input("repetitions: "))
     if code in [100,200]:
-        msg["TYPE"] = input("type:" )
+        msg["TYPE"] = input("type: " )
     if code == 200:
-        msg["VIN"] = float(input("vin"))
-        msg["CHAN"] = int(input("chan:" ))
-    print(f" msg: {msg}")
+        msg["VIN"] = float(input("vin: "))
+        msg["CHAN"] = int(input("chan: " ))
+    if code in [ 300,310,320, 330, 340, 350, 360, 370]:
+        for k, v in funct_desc.items():
+            print(k,v)
+            
+        msg["ARGLIST"] = json.loads(input("arglist: "))      #input returns a stringified list . needs to be a list
+        
+    print(f"\tGUI client sending msg: {msg}")
     return msg  
 
 async def tcp_client():
@@ -50,17 +67,18 @@ async def tcp_client():
     await writer.drain()
     
     #   the get_msg() emulates a GUI with user entering the values to be sent to the server.
-    msg = get_msg()
-    packet = json.dumps(msg) + "\n"
-    writer.write(packet.encode())
-    await writer.drain()
-
-# acts when a msg is sent back to me from Server...
-    data = await reader.readline()
-    dataj = json.loads(data)
-    print("Server responds with :", dataj)
+    # bug fixed: DONE the reader happens first, then another msg is created in get_msg() and sent out.
+    while True:
+        line = await reader.readline()   # blocks until reads "\n"
+        dataj = json.loads(line)
+        print("\tServer says :", dataj)
+        
+        msg = get_msg()
+        packet = json.dumps(msg) + "\n"
+        writer.write(packet.encode())
+        await writer.drain()
 
     #writer.close()
     #await writer.wait_closed()
-
+print(show_dbi_functions())
 asyncio.run(tcp_client())
