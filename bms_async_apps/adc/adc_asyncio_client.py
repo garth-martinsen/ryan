372667b62c51451abdb15e252338cce79c3e6b69
Garth Martinsen <garth.martinsen@gmail.com>
2026-07-03 20:32:17 -0700

    fixed rtc time sync

--- bms_async_apps/adc/adc_asyncio_client.py
+++ bms_async_apps/adc/adc_asyncio_client.py
@@ -12,11 +12,11 @@ adc =  ADC(version)
 svr_ip =  '192.168.254.19',
 svr_port =8888
 reps=0
-reps_remaining=0
+reps_done=0
 period =0
 
 async def route_msg( msg, writer):
-    global reps, reps_remaining
+    global reps, reps_done
     cmd = msg["CODE"]
     msgid = msg.get("MSGID")
     
@@ -49,7 +49,7 @@ async def route_msg( msg, writer):
         reps = msg["REPS"]
         print(f"ADC responding to the 174 cmd (start_periodic_measurements for period: {period} seconds and reps: {reps}")
         reps = reps
-        reps_remaining = reps
+        reps_done = 0
         vin = 0.0     # always vin=0.0 for a measurement but not a calibration
         for cnt in range(reps):
             for chan in range(3):
@@ -57,8 +57,8 @@ async def route_msg( msg, writer):
                 writer.write((json.dumps(response) +"\n").encode())
                 await writer.drain()
             await asyncio.sleep(period)
-            reps_remaining  -= 1
-            print(f"\tFor command 174 reps_remaining: {reps_remaining} of requested reps: {reps}")
+            reps_done  += 1
+            print(f"\tFor command 174 reps_done: {reps_done} of requested reps: {reps}")
         print(f"All {reps} repetitions were completed in start_periodic_measurements (174) cmd")
     # many repeated calibrations, same as 174 except type='c' and vin is the truth value.   
     elif cmd == 274:
@@ -66,13 +66,14 @@ async def route_msg( msg, writer):
         cmd=200
         period = msg["PERIOD"]
         reps = msg["REPS"]
-        print(f"ADC responding to the 174 cmd (start_periodic_measurements for period: {period} seconds and reps: {reps}")
+        vins = msg["VINS"]
+        print(f"ADC responding to the 274 cmd (start_periodic_measurements for period: {period} seconds and reps: {reps}")
         reps = reps
-        reps_remaining = reps
-        vin0= 4.062     # need to measure each chan and set its vin here
-        vin1= 8.05
-        vin2= 12.14
-        vins=[vin0, vin1, vin2]
+        reps_done = 0
+#         vin0= 4.062     # need to measure each chan and set its vin here
+#         vin1= 8.05
+#         vin2= 12.14
+#         vins=[vin0, vin1, vin2]
 
         for cnt in range(reps):
             for chan in range(3):
@@ -80,16 +81,21 @@ async def route_msg( msg, writer):
                 writer.write((json.dumps(response) +"\n").encode())
                 await writer.drain()
             await asyncio.sleep(period)
-            reps_remaining  -= 1
-            print(f"\tFor cmd: 274  the reps_remaining: {reps_remaining} of requested reps: {reps}")
+            reps_done  += 1
+            print(f"\tFor cmd: 274  the reps_done: {reps_done} of requested reps: {reps}")
         print(f"\tAll {reps} repetitions were completed in start_periodic_calibrations (274) cmd")
 
-#TODO 3: Finish/DEBUG block  302 for rtc time setting;
+#TODO 3: Finish/DEBUG block  303 for rtc time setting;
       
     elif cmd == 303:
         print(f"msg: {msg}")
         time = msg["TIME_SYNC"]
-        adc.set_rtc(time)
+        response =adc.set_rtc(time)
+        response = json.dumps(response)+"\n"
+        print(f"ADC response to SVR: {response}")
+        writer.write(response.encode())
+        await writer.drain()
+        
         
 async def tcp_client():   
     reader, writer = await asyncio.open_connection( '192.168.254.19', 8888)
@@ -106,7 +112,7 @@ async def tcp_client():
     while True:
         print("ADC waiting for server message")
         line = await reader.readline()   # blocks until reads a "\n"
-        #print("ADC raw line:", repr(line))
+        print("ADC raw line:", repr(line))
         msg=json.loads(line.decode())
         print("\tADC decoded msg:", msg)
         #TODO 1: DONE : from adc import ADC;   wire it up so I can: 1. call adc.measure(c) where c=chan [0,1,2]; 2. call adc.start_periodic(period, reps).
