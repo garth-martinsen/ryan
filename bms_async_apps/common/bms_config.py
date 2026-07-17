6212ae51bad7ffc144c4dc52a81181a10b017159
Garth Martinsen <garth.martinsen@gmail.com>
2026-07-16 20:22:19 -0700

    Added a common dir right under the proj dir. It is a sibling to svr, gui, adc. added an __init__.py file to id it as a package and added file: bms_config.py. This file will contain project wide data. eg: SSID, SSID_PASSWORD, VERSION, APP_ID, SVR_IP, SVR_PORT, VINS. The vins may be moved to a flet page for input...

---
+++ bms_async_apps/common/bms_config.py
@@ -0,0 +1,16 @@
+#file: bms_config.py
+'''This will be imported by svr, adc_client and gui_client so they can communicate.'''
+import os
+
+print("os is : ", os.getcwd())
+
+SSID = "Ziply1824"
+SSID_PASSWORD = "1wantbr0adband#@!"
+
+SVR_IP =  '192.168.88.2' 
+SVR_PORT =8888
+
+APP_ID = 1
+VERSION = 3
+
+VINS= [4.094, 7.97, 12.01]
