02fd96ca039efa1b1ca13244deff91628bc746d1
Garth Martinsen <garth.martinsen@gmail.com>
2026-07-17 17:54:07 -0700

    updated namedtuples to agree with db schemas. updated func_desc dict : changed 390: from get_vd_fracts to get_estimator_parms

--- bms_async_apps/svr/database_interface_config.py
+++ bms_async_apps/svr/database_interface_config.py
@@ -3,13 +3,13 @@
 from collections import namedtuple
 
 # named tuples... Where they match db table column names they will be upper case.
-
-CONFIG_FIELDS =    ( 'ID', 'OWNER', 'APP_ID', 'APP_DESC', 'CHAN', 'CHAN_DESC', 'VERSION', 'VERSION_DESC',
-                                       'CREATION_TIME', 'TEMPC', 'ADC_FSR', 'ADC_STEPS', 'ADC_SAMPLE_RATE', 'C1', 'R1', 'R2',
-                                       'VD_FRACT', 'LUT_CALIBRATED', 'LUT_TS', 'K_FACTOR')
-
-Config = namedtuple( "Config", CONFIG_FIELDS)                                                                         #19 fields
-
+# APPS: (ID, OWNER,  APP_DESC ,TIMESTAMP  , TEMPC, ADC_FSR, ADC_STEPS, ADC_SAMPLE_RATE, VERSION, VERSION_DESC) 
+APP_CONFIG_FIELDS =    ( 'ID', 'OWNER', 'APP_DESC', 'TIMESTAMP',  'TEMPC', 'ADC_FSR', 'ADC_STEPS', 'ADC_SAMPLE_RATE', 'VERSION', 'VERSION_DESC')
+                                     
+#CHANNELS: ( ID, APP_ID, CHAN,       CHAN_DESC      , VERSION, VERSION_DESC,    TIMESTAMP    ,   C1   ,    R1   ,    R2   ,      SLOPE      , LUT_CALIBRATED,       LUT_TS      , K_FACTOR,     INTERCEPT    )
+CHAN_CONFIG_FIELDS = ( 'ID', 'APP_ID', 'CHAN', 'CHAN_DESC', 'VERSION', 'VERSION_DESC',  'TIMESTAMP',  'C1' ,'R1' , 'R2' ,  'SLOPE'  , 'LUT_CALIBRATED',  'LUT_TS'  , 'K_FACTOR' , 'INTERCEPT')
+APP_CONFIG = namedtuple( "APP_CONFIG", APP_CONFIG_FIELDS )                                                                        #19 fields
+CHAN_CONFIG= namedtuple("CHAN_CONFIG", CHAN_CONFIG_FIELDS)
 Abbrev= namedtuple( "Abbrev", ("id", "owner", "app_desc", "version_desc", "channel_id", "channel_desc"))    #6 fields
 BMS_FIELDS = ("ID", "MSGID", "VERSION", "TIMESTAMP", "TYPE", "CHAN", "A2D_MEAN", "VM_MEAN", "VM_SD", "VB", "VIN", "ERROR", "SAMP_SZ", "DISCARD_SZ", "KEEP_SZ")
 
@@ -37,7 +37,7 @@ funct_desc = {300: 'save_config(  msg : Config )',       302: 'sync_time()',
                        330: 'list_bms( chan:int, atype:str) ',      340: 'get_bms_a2d_samples( bms_id : int)',
                        350: 'get_lut( chan:int)',                          352: 'get_lut_item(chan:int, vin:float)',
                        360: 'get_lut_timestamp( chan:int )',      370:'update_lut_pair(  _id:int,   vm:float,   vin:float)' ,
-                       380: 'update_lut_timestamp( chan:int )', 390: 'get_vd_fracts( )'  }
+                       380: 'update_lut_timestamp( chan:int )', 390: 'get_estimator_parms( )'  }
 
 
 
