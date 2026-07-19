aeaec678576f09f497e93ce5a9d7deff419e4283
Garth Martinsen <garth.martinsen@gmail.com>
2026-07-18 23:30:59 -0700

    Changed svr_ip

--- bms_async_apps/common/bms_config.py
+++ bms_async_apps/common/bms_config.py
@@ -2,9 +2,9 @@
 '''This will be imported by svr, adc_client and gui_client so they can communicate.'''
 import os
 
-print("os is : ", os.getcwd())
+print("bms_config os.getcwd() is : ", os.getcwd())
 
-SVR_IP =  '192.168.88.2' 
+SVR_IP =  '192.168.88.3' 
 SVR_PORT =8888
 
 APP_ID = 1
