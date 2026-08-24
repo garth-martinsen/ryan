116fe79585b1fca095889e74cf4656ab958da07f
Garth Martinsen <garth.martinsen@gmail.com>
2026-08-23 21:04:30 -0700

    Added a2d as an arg to constructor

--- bms_async_apps/adc/ah_meter.py
+++ bms_async_apps/adc/ah_meter.py
@@ -1,24 +1,24 @@
-# file: /adc/ah_meter.py   purpose: accumulate amps measured vi module ammeter and when triggered, return ah_used...
+# file: /adc/ah_meter.py   purpose: accumulate charge  measured via module ammeter and when reporting,put i_mean, ah_used,in the report...
 
 import time
 from collections import OrderedDict
-from typing import List, Integer
-from common.templates.adc_templates import ADC_T0_SVR_TEMPLATE
+#from typing import List, Integer
+from common.templates.adc_templates import ADC_TO_SVR_TEMPLATE
 
 class AhMeter:
     '''Inits as commissioned with LSB, Vtap, a2d, rs,amp_gain  . 
        Starts voltage measurement interval with input_time, sets count=0, and accumulates & counts current measurements until 
        adc_client asks for ah_used, responds by multiplying average current by delta_time to return ah_used.''' 
 
-    def __init__(self, vtap, lsb, rs, amp_gain ):
+    def __init__(self, vtap, lsb, rs, amp_gain, a2d ):
         self.start_time=0.0    # start_time passed in on start(),  end_time is passed in when adc_client asks for ah_used.
         self.amp_sum = 0.0     # accumulates periodic measurements until ah_used(..) is called.
         self.vtap = vtap       # indicates nominal voltage at which ah_used is needed. 12.6V is used for 3Cells in series. Just for show.
         self.count=0           # number of amps measurements added to amp_sum during accumulation phase.
         self.a2d = a2d         # reference to the a2d array stored by the adc on chan[3], for amps.
-        self.lsb =LSB          # for FSR=1.024v and steps =2**15 ; lsb = FSR/steps, 1.024/32768 = 0.00003125 or 31.25µV
+        self.lsb =lsb          # for FSR=1.024v and steps =2**15 ; lsb = FSR/steps, 1.024/32768 = 0.00003125 or 31.25µV
         self.rs = rs           # Measured shunt resistor value in ohms
-        self.amp_gain          # Gain from INA180A1 current sense amplifier eg: 20
+        self.amp_gain = amp_gain         # Gain from INA180A1 current sense amplifier eg: 20
         self.pdict = OrderedDict() # Processing dict
 
     def start(self, start_time:float):
@@ -35,15 +35,17 @@ class AhMeter:
              abin = self.pdict.get(cnt,[])
              abin.append(cnt)
              pdict[cnt]= abin
-         winner = max(pdict.values)
-         keep = [k for k,v in pdict.items() if abs(k-winner)< 2 ]     
-         m = sum(keep)/len(keep)
-         vs= self.lsb * m    #voltage across shunt resistor, rs
-         apparent_amps = (vs/self.rs)                # given vs = 1 V and rs= 0.05123, then apparent_amp_s = (vs/rs)  => apparent_amps = 1.0/.05123 = 19.51981261
-         cs_amps = apparent_amps / self.amp_gain     # now scale down the apparent_amps by amp_gain (20) =>  / amp_gain, so  cs_amp = 19.51981261/20 = 0.97599063 amps. 
-         self.add_amps(cs_amps)
+        winner = max(pdict.values)
+        keep = [k for k,v in pdict.items() if abs(k-winner)< 2 ]     
+        m = sum(keep)/len(keep)
+        vs= self.lsb * m    #voltage across shunt resistor, rs
+        # i=Vs/RS then scale to get true current
+        apparent_amps = (vs/self.rs)                
+        cs_amps = apparent_amps / self.amp_gain     
+        self.add_amps(cs_amps)
 
     def add_amps(self, amps: float):
+        # accumulate and ncrement count so later avg can be returned.
         self.amp_sum += amps
         self.count += 1
 
@@ -61,6 +63,7 @@ class AhMeter:
 
     def report_to_svr(self, report_msg):
         '''Assuming that I can pass in report_msg and populate the amps portion of it and then later pass it to adc.report_to_svr which also updates it.  '''
+        #reporting time is end_time for the report. ie: Now()
         self.end_time= time.time()
         amps_chan = 3
         amps_record  = report_msg["CHANS"][amps_chan]
