b7028562f801cfdf01b6cc3781313d08149e11bd
Garth Martinsen <garth.martinsen@gmail.com>
2026-08-25 22:09:39 -0700

    Added chan to each row in CHANS: as suggested by chatgpt. added comments to explain why A2D array has only two a2d counts.

--- bms_async_apps/common/templates/adc_templates.py
+++ bms_async_apps/common/templates/adc_templates.py
@@ -9,10 +9,10 @@ ADC_TO_SVR_TEMPLATE = {
     "MEAS_ID":1,
     "TYPE":"m",
     "CHANS":[ 
-               {"CHAN":0, "TIMESTAMP": 178000.0, "VIN":0 , "SAMP_SZ": 64, "A2D":[24500,24500]}, 
-               {"CHAN":1, "TIMESTAMP": 178000.0, "VIN":0 , "SAMP_SZ": 64, "A2D":[24500,24500]},
-               {"CHAN":2, "TIMESTAMP": 178000.0, "VIN":0 , "SAMP_SZ": 64, "A2D":[24500,24500]},
-               {"CHAN":3, "TIMESTAMP": 178000.0, "I_MEAN":0.550,"PERIOD_SEC":3600, "AH_USED":0.550 }
+               {"CHAN":0, "TIMESTAMP": 178000.0, "VIN":0 , "SAMP_SZ": 64, "A2D":[24500,24500]},      #A2D will have 64 values.. just dummy here... 
+               {"CHAN":1, "TIMESTAMP": 178000.0, "VIN":0 , "SAMP_SZ": 64, "A2D":[24500,24500]},      #likewise
+               {"CHAN":2, "TIMESTAMP": 178000.0, "VIN":0 , "SAMP_SZ": 64, "A2D":[24500,24500]},      #likewise
+               {"CHAN":3, "TIMESTAMP": 178000.0, "I_MEAN":0.550,"PERIOD_SEC":3600, "AH_USED":0.550 } # values will vary but no arrays...
             ]
 }
 
